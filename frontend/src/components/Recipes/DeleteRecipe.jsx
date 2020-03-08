4159634cb2a33ad0a1a07b5f6c93b7dda993cb3f
mikefeldberg <mikefeldberg@gmail.com>
2020-03-08 09:59:00 -0700

    fix update cache when deleting recipe

--- frontend/src/components/Recipes/DeleteRecipe.jsx
+++ frontend/src/components/Recipes/DeleteRecipe.jsx
@@ -9,32 +9,21 @@ const DeleteRecipe = ({ recipe }) => {
     const currentUser = useContext(AuthContext);
     const isCurrentUser = currentUser.id === recipe.user.id;
 
-// DELETE WORKS; CACHE UPDATE BROKEN
-
-
-const [deleteRecipe] = useMutation(DELETE_RECIPE_MUTATION, {
-    update(cache, { data: { deleteRecipe } }) {
-            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ before data')
+    const [deleteRecipe] = useMutation(DELETE_RECIPE_MUTATION, {
+        update(cache, { data: { deleteRecipe } }) {
             const data = cache.readQuery({ query: GET_RECIPES_QUERY });
-            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ after data')
-            const index = data.recipes.findIndex(recipe => Number(recipe.id) === deleteRecipe.recipeId);
-            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ after index')
+            const index = data.recipes.findIndex(recipe => recipe.id === deleteRecipe.recipeId);
             const recipes = [...data.recipes.slice(0, index), ...data.recipes.slice(index + 1)];
-            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ after recipes')
             cache.writeQuery({
                 query: GET_RECIPES_QUERY,
                 data: { recipes },
             });
-            console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ after write query')
         },
     });
-
-    // const [deleteRecipe, {data}] = useMutation(DELETE_RECIPE_MUTATION);
 
     const handleDelete = async (e, deleteRecipe) => {
         e.preventDefault();
         deleteRecipe({ variables: { recipeId: recipe.id } });
-        console.log('handling delete');
     };
 
     return (
