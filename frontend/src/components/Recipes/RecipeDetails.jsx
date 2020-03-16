e67dc10d2c1b9023a57d66f0cecccc372011765f
mikefeldberg <mikefeldberg@gmail.com>
2020-03-16 13:40:44 -0700

    disable recipedetails toolbar for unauthorized users

--- frontend/src/components/Recipes/RecipeDetails.jsx
+++ frontend/src/components/Recipes/RecipeDetails.jsx
@@ -42,30 +42,32 @@ const RecipeDetails = ({ match, history }) => {
         // debugger
         return (
             <>
-                <Row>
-                    <ButtonToolbar className="mb-2">
-                        <Link to={`/recipes/${recipe.id}/edit`} className="mr-2">
-                            <Button size="sm" variant="secondary-inverse">
-                                {<i className="fas fa-edit text-secondary"></i>}
-                            </Button>
-                        </Link>
-                        <DropdownButton
-                            size="sm"
-                            drop="right"
-                            title={<i className="fas fa-trash text-danger"></i>}
-                            variant="danger-inverse"
-                        >
-                            <Dropdown.Item disabled eventKey="0">
-                                Are you sure?
-                            </Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Item eventKey="1">
-                                <DeleteRecipe history={history} recipe={recipe} />
-                            </Dropdown.Item>
-                            <Dropdown.Item eventKey="2">Cancel</Dropdown.Item>
-                        </DropdownButton>
-                    </ButtonToolbar>
-                </Row>
+                {recipe.user === currentUser &&
+                    <Row>
+                        <ButtonToolbar className="mb-2">
+                            <Link to={`/recipes/${recipe.id}/edit`} className="mr-2">
+                                <Button size="sm" variant="secondary-inverse">
+                                    {<i className="fas fa-edit text-secondary"></i>}
+                                </Button>
+                            </Link>
+                            <DropdownButton
+                                size="sm"
+                                drop="right"
+                                title={<i className="fas fa-trash text-danger"></i>}
+                                variant="danger-inverse"
+                            >
+                                <Dropdown.Item disabled eventKey="0">
+                                    Are you sure?
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item eventKey="1">
+                                    <DeleteRecipe history={history} recipe={recipe} />
+                                </Dropdown.Item>
+                                <Dropdown.Item eventKey="2">Cancel</Dropdown.Item>
+                            </DropdownButton>
+                        </ButtonToolbar>
+                    </Row>
+                }
                 <Image src={recipe.photos.length > 0 ? recipe.photos[0].url : `https://cookbook-test-bucket.s3-us-west-1.amazonaws.com/_food_placeholder.jpg`} fluid />
                 <div>Title: {recipe.title}</div>
                 <div>
