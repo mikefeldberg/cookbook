5ca0390571956563cd5d140ebc94e601f3455554
mikefeldberg <mikefeldberg@gmail.com>
2020-03-16 16:10:25 -0700

    wip

---
+++ frontend/src/components/Comments/CommentSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const CommentSection = ({comments}) => {
+    return (
+        <div>
+            Here there be comments
+            {/* <NewComment> */}
+            {/* <Comments> */}
+        </div>
+    );
+}
+ 
+export default CommentSection;
