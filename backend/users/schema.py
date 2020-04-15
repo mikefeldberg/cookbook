16a4da94630cfa293c4388f853eac68618908828
Mike Feldberg <mikefeldberg@gmail.com>
2020-04-14 21:50:28 -0700

    update filtersets to filter deleted recipes, comments, favorites (#28)

--- backend/users/schema.py
+++ backend/users/schema.py
@@ -2,12 +2,52 @@ from django.contrib.auth import get_user_model
 from graphql import GraphQLError
 
 import graphene
+from recipes.models import Recipe, Comment, Favorite
 from graphene_django import DjangoObjectType
+from django_filters import FilterSet
+
+class RecipeFilter(FilterSet):
+    class Meta:
+        model = Recipe
+        fields = ['created_at', 'deleted_at']
+
+    @property
+    def qs(self):
+        return super().qs.filter(deleted_at=None)
+
+
+class CommentFilter(FilterSet):
+    class Meta:
+        model = Comment
+        fields = ['created_at', 'deleted_at']
+
+    @property
+    def qs(self):
+        return super().qs.filter(deleted_at=None)
+
+
+class FavoriteFilter(FilterSet):
+    class Meta:
+        model = Favorite
+        fields = ['created_at', 'deleted_at']
+
+    @property
+    def qs(self):
+        return super().qs.filter(deleted_at=None)
 
 
 class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
+
+    def resolve_recipe_set(self, info, **kwargs):
+        return RecipeFilter(kwargs).qs
+
+    def resolve_comment_set(self, info, **kwargs):
+        return CommentFilter(kwargs).qs
+
+    def resolve_favorite_set(self, info, **kwargs):
+        return FavoriteFilter(kwargs).qs
 
 
 class Query(graphene.ObjectType):
