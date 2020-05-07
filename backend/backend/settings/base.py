9328614a4aea74937866863e83f6b27867279116
mikefeldberg <mikefeldberg@gmail.com>
2020-05-06 21:36:47 -0700

    add requirements file

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api']
+# ALLOWED_HOSTS = ['api']
 
 ALLOWED_HOSTS = ['api', 'localhost']
 
