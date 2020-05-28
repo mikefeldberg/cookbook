c4a2ce1c37e667262fc445327f54ee900321f09f
mikefeldberg <mikefeldberg@gmail.com>
2020-05-27 21:45:26 -0700

    fix

--- frontend/src/components/Shared/UserAvatar.jsx
+++ frontend/src/components/Shared/UserAvatar.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const UserAvatar = ({ user, size }) => {
+const UserAvatar = ({ user, size, showLabel = false }) => {
     return (
         <Link style={{ textDecoration: 'none' }} to={`/profile/${user.username}`}>
             <div className={'user-avatar user-avatar-' + size}
                  style={{ backgroundImage: `url(${user.photos.length > 0 ? user.photos[0].url : "/avatar_placeholder.png"})` }}></div>
-            { size == 'sm' && <span className="link">{user.username}</span> }
+            { showLabel && <span className="link">{user.username}</span> }
         </Link>
     );
 };
